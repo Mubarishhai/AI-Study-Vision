ef95f137324a6acb5554dd55a157c1aed002fff5
Shaikh Mubarish <mubarishshaikh2@gmail.com>
2025-11-30 15:58:46 +0530

    optimizw code

--- doubt_scanner/scanner.py
+++ doubt_scanner/scanner.py
@@ -1,19 +1,8 @@
-from paddleocr import PaddleOCR
+import pytesseract
 from PIL import Image
-import numpy as np
+import io
 
-# Load OCR only once
-ocr = PaddleOCR(lang='en', use_angle_cls=True)
-
-def scan_doubt(image_path):
-    img = Image.open(image_path).convert("RGB")
-    img_np = np.array(img)
-
-    result = ocr.ocr(img_np, cls=True)
-
-    text_output = []
-    for line in result:
-        for part in line:
-            text_output.append(part[1][0])
-
-    return "\n".join(text_output)
+def scan_doubt(image_file):
+    img = Image.open(io.BytesIO(image_file.read()))
+    text = pytesseract.image_to_string(img)
+    return text
